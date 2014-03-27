8b103536ff8b36d02b7f9e4e7327737c565029ed
dima888 <dima888@hotmail.de>
2014-03-27 21:03:29 +0100

    deleteVertex erweitert, dass auch die Kanten automatisch mit geloescht werden.

--- GKA_PRAKTIKUM/src/graph_adt.erl
+++ GKA_PRAKTIKUM/src/graph_adt.erl
@@ -48,10 +48,14 @@ deleteVertex(V_ID, Graph) ->
 VertexList = [ lists:nth(2, X) || X <- Vertices], %Alle Vertex IDs
 BoolDoubleVertex = lists:member(V_ID, VertexList),
 
-%----------------- IMPL --------------------
+%----------------- IMPL --------------------	
 ModifyVertexList = [ X || X <- Vertices, lists:nth(2, X) =/= V_ID],
 	if not BoolDoubleVertex-> nil; %Hier waerte schoener explizit zusagen, wo der Fehler ist, ist einfach freundlicher!
-	true -> ModifyGraph = {ModifyVertexList, EdgesD, EdgesU}
+	true ->
+		%Wir loeschen alle Kanten, die an den Vertex haengen
+ModifyEdgesD  = [ X || X <- EdgesD, ( element(1, lists:nth(2, X)) =/= V_ID ) and ( element(2, lists:nth(2, X)) =/= V_ID )],
+ModifyEdgesU  = [ X || X <- EdgesD, ( element(1, lists:nth(2, X)) =/= V_ID ) and ( element(2, lists:nth(2, X)) =/= V_ID )],
+		ModifyGraph = {ModifyVertexList, ModifyEdgesD, ModifyEdgesU}
 	end.
 
 %---------------- METHODE ---------------------
@@ -83,7 +87,6 @@ addEdgeD(V_ID1, V_ID2, Graph) ->
 	
 	
 %Normalerweise muesste auch das gleiche noch mal fuer EdgeD geprueft werden	
-
 if (not (Bool_V_ID1 and Bool_V_ID2)) -> nil;
 	true -> { Vertices, ModifyEdgeD = EdgesD ++ [[edgeD, {V_ID1, V_ID2}] ], EdgesU }
 end.
@@ -113,3 +116,4 @@ deleteEdge(V_ID1, V_ID2, Graph) ->
 	%G5 = graph_adt:addEdgeD(1, 3, G4). 
 	%G6 = graph_adt:addEdgeU(1, 3, G5).
 	%G7 = graph_adt:deleteEdge(1, 2, G6).
+	%TestGraph = {[[vertex,1],[vertex,2],[vertex,3]], [[edgeD,{1,2}],[edgeD,{1,3}]], [[edgeU,{1,3}]]}
