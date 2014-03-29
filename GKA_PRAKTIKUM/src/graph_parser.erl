4e9cff70729ea4205b4bc259e7da8fe955510e7c
dima888 <dima888@hotmail.de>
2014-03-29 15:03:31 +0100

    parser angefangen zu implementieren

--- GKA_PRAKTIKUM/src/graph_parser.erl
+++ GKA_PRAKTIKUM/src/graph_parser.erl
@@ -7,17 +7,66 @@
 %% ====================================================================
 %% API functions
 %% ====================================================================
--export([importGraph/2]).
+-export([importGraph/2, readlines/1, costImport/1, costDirektedImport/4]).
 
 
 
 %% ====================================================================
 %% Internal functions
 %% ====================================================================
-
-importGraph(Datei, Attr) -> 
+%TODO: FilePath verlagern || FilePath = Datei
+importGraph(Datei, Attr) when Attr == "cost" ->	
+	costImport(Datei).
+	 
+%---------- HILFS FUNKTION ------------
+costImport(Datei) -> 
+	Graph = graph_adt:new_AlGraph(),
+	%Datei = "C:\\Users\\foxhound\\Desktop\\test.txt",
 	
-	FilePath = "C:\\Users\\foxhound\\Desktop\\test2.txt",
-	TupleData = file:read_file(FilePath),
-	ListDataAlpha = tuple_to_list(TupleData),
-	Data = lists:nth(2, ListDataAlpha).
+	{ok, Device} = file:open(Datei, [read]),
+	
+	%Erste Zeile, wo angegeben wird ob es gerichtet oder ungerichtet ist
+	Row = io:get_line(Device, []),
+	
+	%Unsere IDs fangen bei 1 an
+	if Row == "#gerichtet\n" -> costDirektedImport(Datei, Graph, 1, Device);
+	   true -> constUndirektedImport(Datei)
+	end.
+
+%---------- HILFS FUNKTION ------------
+costDirektedImport(Datei, Graph, V_ID1, Device) ->
+
+	%FilePath = "C:\\Users\\foxhound\\Desktop\\test.txt",
+	%{ok, Device} = file:open(Datei, [read]),
+	%%Row = io:get_line(Device, []),
+	Row = io:get_line(Device, []),
+	
+	PartGraph = string:tokens(Row, ", \n "),
+	
+	%Alle Attribute aus der Textdatei raus hollen
+	SourceAttrName = lists:nth(1, PartGraph),
+	TargetAttrName = lists:nth(2, PartGraph),
+	CoustAttr = lists:nth(3, PartGraph).
+
+	%Hier kommen die Pruefungen ob die IDs in den Graphen rein duerfen
+	
+
+	%Graph wird befuelt
+	
+	
+	
+%---------- HILFS FUNKTION ------------
+constUndirektedImport(Datei) -> 
+	X = 25.
+
+readlines(FileName) ->
+    {ok, Device} = file:open(FileName, [read]),
+    try get_all_lines(Device)
+      after file:close(Device)
+    end.
+
+get_all_lines(Device) ->
+    case io:get_line(Device, "") of
+        eof  -> [];
+        Line -> Line ++ get_all_lines(Device)
+    end.
