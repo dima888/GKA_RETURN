665bbb718db499789eb6a45972bbb3b534e3730e
dima888 <dima888@hotmail.de>
2014-03-30 17:04:00 +0200

    Parser mit 3 Parameter fast implementiert, es fehlt nur noch die Abbruchbedingung und noch eine kleinigkeit. Jedoch bin ich der Meinung, die kleinigkeit sollte nich implementiert werden, aber Klauck laesst in seinen beispiel Graphen sowas hier  zu: v, s, 10 s, v, 20 . Da frage ich mich schon was das soll?

--- GKA_PRAKTIKUM/src/graph_parser.erl
+++ GKA_PRAKTIKUM/src/graph_parser.erl
@@ -7,7 +7,7 @@
 %% ====================================================================
 %% API functions
 %% ====================================================================
--export([importGraph/2, readlines/1, costImport/1, costDirektedImport/4]).
+-export([importGraph/2, readlines/1, costImport/1, costDirektedImport/5]).
 
 
 
@@ -26,44 +26,100 @@ costImport(Datei) ->
 	%Erste Zeile, wo angegeben wird ob es gerichtet oder ungerichtet ist
 	Row = io:get_line(Device, []),
 	
-	%Unsere IDs fangen bei 1 an, hier 0, da es in der verarbeitung so angegeben wird: V_ID++
-	if Row == "#gerichtet\n" -> costDirektedImport(Datei, graph_adt:new_AlGraph(), 0, Device);
+	%IDs werden immer plus 2 genommen, somit vermeiden wir gleiche Ids
+	if Row == "#gerichtet\n" -> costDirektedImport(Datei, graph_adt:new_AlGraph(), 1, 2, Device);
 	   true -> constUndirektedImport(Datei)
 	end.
 
 %---------- HILFS FUNKTION ------------
-costDirektedImport(Datei, Graph, V_ID, Device) ->
-
+%TODO: Richtige Abbruch bedingung implementieren
+costDirektedImport(Datei, Graph, V_ID1, V_ID2, Device) when V_ID1 == 9 -> Graph;
+costDirektedImport(Datei, Graph, V_ID1, V_ID2, Device) ->
 	%FilePath = "C:\\Users\\foxhound\\Desktop\\test.txt",
-	%{ok, Device} = file:open(Datei, [read]),
-	%%Row = io:get_line(Device, []),
 	Row = io:get_line(Device, []),
+	io:write(Row),
 	
 	PartGraph = string:tokens(Row, ", \n "),
 	
-	%Alle Attribute aus der Textdatei raus hollen
+	%Alle AttributeValue aus der Textdatei raus hollen
 	SourceValName = lists:nth(1, PartGraph),
 	TargetValName = lists:nth(2, PartGraph),
 	CoustVal = lists:nth(3, PartGraph),
 
-	%Hier kommen die Pruefungen ob die IDs in den Graphen rein duerfen
+	%Hier kommen die Pruefungen ob die IDs in den Graphen rein duerfen TODO: Keine Ahnung ob ich das noch brauche
 	BoolValVertexFirst = graph_adt:includeValue(SourceValName, Graph),
 	BoolValVertexSecond = graph_adt:includeValue(TargetValName, Graph),
- 
 	
-	%%TODO: Hier bin ich stehen geblieben
-	if BoolValVertexFirst ->
-		   ID = V_ID + 1,
-		   graph_adt:addvertex(ID, Graph);
-	   BoolValVertexSecond -> 
-		   ID2 = V_ID + 2,
-		   graph_adt:addvertex(ID2, Graph);
-	   true -> nil
-	end,
+	if ( (BoolValVertexFirst  == true) and (BoolValVertexSecond == true)  ) ->
+		   %Der Fall ist unsinn, also waehre unsinnig von benutzer so einen graphen zu erstellen, 
+		   %in diesen Fall packt er ein und die gleiche Kante doppeln rein. Deshalb behandele ich diesen Fall erstmal nicht!
+		   X = unsinn;
+		   
+	   %In diesen Fall ist die V_ID1 schon vorhanden und V_ID2 noch nicht
+	   ( (BoolValVertexFirst  == true) and (BoolValVertexSecond == false)  ) -> 
+		   	%Vertex an zweiter Stelle in .graph hinzu fuegen
+			ModifyGraph = graph_adt:addVertex(V_ID2, Graph),
+			
+			%Dem Vertex das Attribut hinzu kleben
+			ModifyGraph_2 = graph_adt:setValV(V_ID2, name, TargetValName, ModifyGraph),
 	
-	X = 4.
+			%Die schon vorhandene ID ausgraben
+			Buffer = graph_adt:getIDFromAttrValue(SourceValName, ModifyGraph_2),
+			ExistingID = lists:nth(1, Buffer),
+			
+			%Kante hinzu fuegen
+			ModifyGraph_3 = graph_adt:addEdgeD(ExistingID, V_ID2, ModifyGraph_2),
+			
+			%Attribut an die Kante kleben
+			ModifyGraph_4 = graph_adt:setValE({ExistingID, V_ID2}, cost, CoustVal, ModifyGraph_3),
+			
+			%Zurueck in die Rekursion
+			costDirektedImport(Datei, ModifyGraph_4, V_ID1 + 2, V_ID2 + 2, Device);
+	   
+	   %In diesen Fall ist die V_ID2 schon vorhanden und V_ID1 noch nicht
+	   ( (BoolValVertexFirst  == false) and (BoolValVertexSecond == true)  ) ->
+		   %Vertex an erster Stelle in .graph hinzu fuegen
+		   ModifyGraph = graph_adt:addVertex(V_ID1, Graph),
+		   
+		   	%Dem Vertex das Attribut hinzu kleben
+			ModifyGraph_2 = graph_adt:setValV(V_ID1, name, TargetValName, ModifyGraph),
+		   
+		   	%Die schon vorhandene ID ausgraben
+			Buffer = graph_adt:getIDFromAttrValue(SourceValName, ModifyGraph_2),
+			ExistingID = lists:nth(1, Buffer),
+		   
+		   %Kante hinzu fuegen
+			ModifyGraph_3 = graph_adt:addEdgeD(V_ID1, ExistingID, ModifyGraph_2),
+		   
+		   	%Attribut an die Kante kleben
+			ModifyGraph_4 = graph_adt:setValE({V_ID1, ExistingID}, cost, CoustVal, ModifyGraph_3),
+		   
+		   %Zurueck in die Rekursion
+		   costDirektedImport(Datei, ModifyGraph_4, V_ID1 + 2, V_ID2 + 2, Device);
+		   
+	   %%Das ist immer der Fall, wenn beide Vertices IDs im Graphen noch nicht vorhanden sind
+	   true -> 		   		   
+		   	%Verteces hinzu gefuegt
+			ModifyGraph = graph_adt:addVertex(V_ID1, Graph), 
+			
+			%Dem Vertex das Attribut hinzu kleben
+			ModifyGraph_2 = graph_adt:setValV(V_ID1, name, SourceValName, ModifyGraph),
 
-	%Graph wird befuelt
+			%Zweiten Vertex hinzu fuegen
+			ModifyGraph_3 = graph_adt:addVertex(V_ID2, ModifyGraph_2),
+			
+			%Dem Vertex das Attribut hinzu kleben
+			ModifyGraph_4 = graph_adt:setValV(V_ID2, name, TargetValName, ModifyGraph_3),
+	
+			%Kante hinzu fuegen
+			ModifyGraph_5 = graph_adt:addEdgeD(V_ID1, V_ID2, ModifyGraph_4),
+			
+			%Attribut an die Kante kleben
+			ModifyGraph_6 = graph_adt:setValE({V_ID1, V_ID2}, cost, CoustVal, ModifyGraph_5),
+	
+			%Zurueck in die Rekursion
+			costDirektedImport(Datei, ModifyGraph_6, V_ID1 + 2, V_ID2 + 2, Device)
+		   end.
 	
 	
 	
