bfe01d8a019bf9c9e860b9dcbe308abed8e6e162
dima888 <dima888@hotmail.de>
2014-03-31 22:15:23 +0200

    Parser mit 1 Attribut fuer gerichtet und ungerichtet fertig implementiert.

--- GKA_PRAKTIKUM/src/graph_parser.erl
+++ GKA_PRAKTIKUM/src/graph_parser.erl
@@ -7,7 +7,8 @@
 %% ====================================================================
 %% API functions
 %% ====================================================================
--export([importGraph/2, readlines/1, costImport/1, costDirektedImport/5]).
+-export([importGraph/2, readlines/1, costImport/1, costDirektedImport/6, constUndirektedImport/6,
+		  countLines/1 ]).
 
 
 
@@ -27,18 +28,25 @@ costImport(Datei) ->
 	Row = io:get_line(Device, []),
 	
 	%IDs werden immer plus 2 genommen, somit vermeiden wir gleiche Ids
-	if Row == "#gerichtet\n" -> costDirektedImport(Datei, graph_adt:new_AlGraph(), 1, 2, Device);
-	   true -> constUndirektedImport(Datei)
+	if Row == "#gerichtet\n" -> costDirektedImport(Datei, graph_adt:new_AlGraph(), 1, 2, Device, 1);
+	   Row == "#ungerichtet\n" -> constUndirektedImport(Datei, graph_adt:new_AlGraph(), 1, 2, Device, 1);
+	   true -> format_unknow
 	end.
 
-%---------- HILFS FUNKTION ------------
-%TODO: Richtige Abbruch bedingung implementieren
-costDirektedImport(Datei, Graph, V_ID1, V_ID2, Device) when V_ID1 == 15 -> Graph;
-costDirektedImport(Datei, Graph, V_ID1, V_ID2, Device) ->
+%--------------- ALGORITHMUS ZUM IMPORTIEREN EINES GERICHTETEN GRAPHEN ------------------
+costDirektedImport(Datei, Graph, V_ID1, V_ID2, Device, Count) ->
+	
+	%---------- ABBRUCHBEDINGUNG -----------------
+	LinesNumber = graph_parser:countLines(Datei),
+	if ( Count == LinesNumber) -> 
+		   Graph; 
+	   true -> 
+	
+	
 	%FilePath = "C:\\Users\\foxhound\\Desktop\\test.txt",
-	%FilePath = "C:\\Users\\foxhound\\Desktop\\Beispiel3.txt",
+	%FilePath = "C:\\Users\\foxhound\\Desktop\\Beispiel.txt",
+	%"C:\\Users\\foxhound\\Desktop\\beispiel.txt"
 	Row = io:get_line(Device, []),
-%% 	io:write(Row),
 	
 	PartGraph = string:tokens(Row, ", \n "),
 	
@@ -70,7 +78,7 @@ costDirektedImport(Datei, Graph, V_ID1, V_ID2, Device) ->
 			ModifyGraph_2 = graph_adt:setValE({ExistingID_1, ExistingID_2}, cost, CoustVal, ModifyGraph),
 			
 			%Zurueck in die Rekursion
-			costDirektedImport(Datei, ModifyGraph_2, V_ID1 + 2, V_ID2 + 2, Device);
+			costDirektedImport(Datei, ModifyGraph_2, V_ID1 + 2, V_ID2 + 2, Device, Count + 1);
 		   
 	   %In diesen Fall ist die V_ID1 schon vorhanden und V_ID2 noch nicht
 	   ( (BoolValVertexFirst  == true) and (BoolValVertexSecond == false)  ) -> 
@@ -91,7 +99,7 @@ costDirektedImport(Datei, Graph, V_ID1, V_ID2, Device) ->
 			ModifyGraph_4 = graph_adt:setValE({ExistingID, V_ID2}, cost, CoustVal, ModifyGraph_3),
 			
 			%Zurueck in die Rekursion
-			costDirektedImport(Datei, ModifyGraph_4, V_ID1 + 2, V_ID2 + 2, Device);
+			costDirektedImport(Datei, ModifyGraph_4, V_ID1 + 2, V_ID2 + 2, Device, Count + 1);
 	   
 	   %In diesen Fall ist die V_ID2 schon vorhanden und V_ID1 noch nicht
 	   ( (BoolValVertexFirst  == false) and (BoolValVertexSecond == true)  ) ->
@@ -112,7 +120,7 @@ costDirektedImport(Datei, Graph, V_ID1, V_ID2, Device) ->
 			ModifyGraph_4 = graph_adt:setValE({V_ID1, ExistingID}, cost, CoustVal, ModifyGraph_3),
 		   
 		   %Zurueck in die Rekursion
-		   costDirektedImport(Datei, ModifyGraph_4, V_ID1 + 2, V_ID2 + 2, Device);
+		   costDirektedImport(Datei, ModifyGraph_4, V_ID1 + 2, V_ID2 + 2, Device, Count + 1);
 		   
 	   %%Das ist immer der Fall, wenn beide Vertices IDs im Graphen noch nicht vorhanden sind
 	   true -> 		   		   
@@ -135,14 +143,121 @@ costDirektedImport(Datei, Graph, V_ID1, V_ID2, Device) ->
 			ModifyGraph_6 = graph_adt:setValE({V_ID1, V_ID2}, cost, CoustVal, ModifyGraph_5),
 	
 			%Zurueck in die Rekursion
-			costDirektedImport(Datei, ModifyGraph_6, V_ID1 + 2, V_ID2 + 2, Device)
-		   end.
+			costDirektedImport(Datei, ModifyGraph_6, V_ID1 + 2, V_ID2 + 2, Device, Count + 1)
+		    end
 	
+	end.
 	
+%--------------- ALGORITHMUS ZUM IMPORTIEREN EINES UNGERICHTETEN GRAPHEN ------------------
+constUndirektedImport(Datei, Graph, V_ID1, V_ID2, Device, Count) -> 
+		%---------- ABBRUCHBEDINGUNG -----------------
+	LinesNumber = graph_parser:countLines(Datei),
+	if ( Count == LinesNumber) -> 
+		   Graph; 
+	   true -> 
 	
-%---------- HILFS FUNKTION ------------
-constUndirektedImport(Datei) -> 
-	X = 25.
+	%graph_parser:costImport("C:\\Users\\foxhound\\Desktop\\ungerichtet.txt").
+	Row = io:get_line(Device, []),
+	
+	PartGraph = string:tokens(Row, ", \n "),
+	
+	%Alle AttributeValue aus der Textdatei raus hollen, Hier spielt Source und Target keine Rolle
+	SourceValName = lists:nth(1, PartGraph),
+	TargetValName = lists:nth(2, PartGraph),
+	CoustVal = lists:nth(3, PartGraph),
+
+	%Hier kommen die Pruefungen ob die IDs in den Graphen rein duerfen TODO: Keine Ahnung ob ich das noch brauche
+	BoolValVertexFirst = graph_adt:includeValue(SourceValName, Graph),
+	BoolValVertexSecond = graph_adt:includeValue(TargetValName, Graph),
+	
+	if ( (BoolValVertexFirst  == true) and (BoolValVertexSecond == true)  ) -> 
+		   %TODO: Lesen und beraten, gegebenfalls in der Vorlesung ansprechen ob wir diesen Fall wirklich benoetigen
+		   %in diesen Fall packt er ein und die gleiche Kante doppeln rein. Deshalb behandele ich diesen Fall erstmal nicht!
+	
+		   %Die schon vorhandene ID ausgraben
+			Buffer = graph_adt:getIDFromAttrValue(SourceValName, Graph),
+			ExistingID_1 = lists:nth(1, Buffer),
+		   
+			%Die schon vorhandene ID ausgraben
+			Buffer_2 = graph_adt:getIDFromAttrValue(TargetValName, Graph),
+			ExistingID_2 = lists:nth(1, Buffer_2),
+			
+			%Kante hinzu fuegen
+			ModifyGraph = graph_adt:addEdgeU(ExistingID_1, ExistingID_2, Graph),
+			
+			%Attribut an die Kante kleben
+			ModifyGraph_2 = graph_adt:setValE({ExistingID_1, ExistingID_2}, cost, CoustVal, ModifyGraph),
+			
+			%Zurueck in die Rekursion
+			constUndirektedImport(Datei, ModifyGraph_2, V_ID1 + 2, V_ID2 + 2, Device, Count + 1);
+		   
+	   %In diesen Fall ist die V_ID1 schon vorhanden und V_ID2 noch nicht
+	   ( (BoolValVertexFirst  == true) and (BoolValVertexSecond == false)  ) -> 
+		   	%Vertex an zweiter Stelle in .graph hinzu fuegen
+			ModifyGraph = graph_adt:addVertex(V_ID2, Graph),
+			
+			%Dem Vertex das Attribut hinzu kleben
+			ModifyGraph_2 = graph_adt:setValV(V_ID2, name, TargetValName, ModifyGraph),
+	
+			%Die schon vorhandene ID ausgraben
+			Buffer = graph_adt:getIDFromAttrValue(SourceValName, ModifyGraph_2),
+			ExistingID = lists:nth(1, Buffer),
+			
+			%Kante hinzu fuegen
+			ModifyGraph_3 = graph_adt:addEdgeU(ExistingID, V_ID2, ModifyGraph_2),
+			
+			%Attribut an die Kante kleben
+			ModifyGraph_4 = graph_adt:setValE({ExistingID, V_ID2}, cost, CoustVal, ModifyGraph_3),
+			
+			%Zurueck in die Rekursion
+			constUndirektedImport(Datei, ModifyGraph_4, V_ID1 + 2, V_ID2 + 2, Device, Count + 1);
+	   
+	   %In diesen Fall ist die V_ID2 schon vorhanden und V_ID1 noch nicht
+	   ( (BoolValVertexFirst  == false) and (BoolValVertexSecond == true)  ) ->
+		   %Vertex an erster Stelle in .graph hinzu fuegen
+		   ModifyGraph = graph_adt:addVertex(V_ID1, Graph),
+		   
+		   	%Dem Vertex das Attribut hinzu kleben
+			ModifyGraph_2 = graph_adt:setValV(V_ID1, name, TargetValName, ModifyGraph),
+		   
+		   	%Die schon vorhandene ID ausgraben
+			Buffer = graph_adt:getIDFromAttrValue(SourceValName, ModifyGraph_2),
+			ExistingID = lists:nth(1, Buffer),
+		   
+		   %Kante hinzu fuegen
+			ModifyGraph_3 = graph_adt:addEdgeU(V_ID1, ExistingID, ModifyGraph_2),
+		   
+		   	%Attribut an die Kante kleben
+			ModifyGraph_4 = graph_adt:setValE({V_ID1, ExistingID}, cost, CoustVal, ModifyGraph_3),
+		   
+		   %Zurueck in die Rekursion
+		   constUndirektedImport(Datei, ModifyGraph_4, V_ID1 + 2, V_ID2 + 2, Device, Count + 1);
+		   
+	   %%Das ist immer der Fall, wenn beide Vertices IDs im Graphen noch nicht vorhanden sind
+	   true -> 		   		   
+		   	%Verteces hinzu gefuegt
+			ModifyGraph = graph_adt:addVertex(V_ID1, Graph), 
+			
+			%Dem Vertex das Attribut hinzu kleben
+			ModifyGraph_2 = graph_adt:setValV(V_ID1, name, SourceValName, ModifyGraph),
+
+			%Zweiten Vertex hinzu fuegen
+			ModifyGraph_3 = graph_adt:addVertex(V_ID2, ModifyGraph_2),
+			
+			%Dem Vertex das Attribut hinzu kleben
+			ModifyGraph_4 = graph_adt:setValV(V_ID2, name, TargetValName, ModifyGraph_3),
+	
+			%Kante hinzu fuegen
+			ModifyGraph_5 = graph_adt:addEdgeU(V_ID1, V_ID2, ModifyGraph_4),
+			
+			%Attribut an die Kante kleben
+			ModifyGraph_6 = graph_adt:setValE({V_ID1, V_ID2}, cost, CoustVal, ModifyGraph_5),
+	
+			%Zurueck in die Rekursion
+			constUndirektedImport(Datei, ModifyGraph_6, V_ID1 + 2, V_ID2 + 2, Device, Count + 1)
+		    end
+	
+	end.
 
 readlines(FileName) ->
     {ok, Device} = file:open(FileName, [read]),
@@ -155,3 +270,14 @@ get_all_lines(Device) ->
         eof  -> [];
         Line -> Line ++ get_all_lines(Device)
     end.
+
+countLines(File) ->
+	Line = readlines(File),
+%"#gerichtet\ns,u,10\ns,x,5\nu,v,1\nu,x,2\nx,u,3\nx,y,2\ny,s,7\ny,v,6\nv,y,4"
+%"C:\\Users\\foxhound\\Desktop\\test.txt"
+	Z = [ [X] || X <- Line],
+	Res = [ X || X <- Z, X == "\n"],
+	L = length(Res) + 1 .
+	
+	
+	
